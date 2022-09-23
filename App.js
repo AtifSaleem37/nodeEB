b5089c045a575de9b95a772687e6ba44b648717e
AtifSaleem37 <108580110+AtifSaleem37@users.noreply.github.com>
2022-09-23 18:08:49 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Updated");
+    res.send("Updated 1.0.0");
 });
 
 const port = process.env.port || 3000;
