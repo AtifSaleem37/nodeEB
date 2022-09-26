af0c614d037e2ef9f99d4746e2f705d9832bc7ed
AtifSaleem37 <108580110+AtifSaleem37@users.noreply.github.com>
2022-09-26 15:41:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Atif Saleem");
+    res.send("Successfully deployed node application");
 });
 const port = process.env.port || 8080
 app.listen(port, () => {
